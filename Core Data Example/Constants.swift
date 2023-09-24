13982a4e071d70e0020c287dfdf1e4a0e47752d7
Fatih Şükran <fth.21.81@gmail.com>
2023-09-24 09:56:49 +0300

    constants

---
+++ Core Data Example/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Core Data Example
+//
+//  Created by fatih.sukran on 23.09.2023.
+//
+
+import Foundation
+
+let addedNewStudent = Notification.Name("AddedNewStudent")
+let detailPageSegue = "DetailPageSegue"
+let studentEntityName = "Student"
